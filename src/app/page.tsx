8b892d55b6829043c95cecf18caa10d5ecf8694d
Darshan Pawar <darshanpawar010@gmail.com>
2024-10-18 02:53:53 +0530

    Event Scheduling Web App Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import {
+  Calendar,
+  CalendarRange,
+  Globe,
+  Link,
+  Link2,
+  Mail,
+} from "lucide-react";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default function HomePage() {
+  const { userId } = auth();
+  if (userId != null) redirect("/events");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen flex flex-col">
+      <div className="text-center container my-4 mx-auto">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2 font-semibold mr-auto ">
+            <CalendarRange className="size-6" />
+            <span className="sr-only md:not-sr-only">Calendly</span>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              asChild
+              variant={"ghost"}
+              className="border-2 border-blue-500 text-blue-700"
+            >
+              <SignInButton />
+            </Button>
+            <Button
+              asChild
+              variant={"ghost"}
+              className="bg-blue-500 text-white"
+            >
+              <SignUpButton />
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="grid gap-10 lg:grid-cols-[repeat(4,1fr)] md:grid-cols-[repeat(2,1fr)] my-40">
+          <div className="flex flex-col gap-5 items-center border-2 px-6 py-4 rounded-2xl">
+            <div className="bg-blue-100 p-4 rounded-full">
+              <Calendar className="size-7 text-blue-500" />
+            </div>
+            <div className="text-xl font-bold text-blue-600">
+              Easy Scheduling
+            </div>
+            <div>
+              Quickly set up and manage events with a simple and intuitive
+              interface. Users can choose their availability, and invitees can
+              easily book a time that works for everyone.
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 items-center border-2 px-6 py-4 rounded-2xl">
+            <div className="bg-pink-100 p-4 rounded-full">
+              <Link className="size-7 text-pink-500" />
+            </div>
+            <div className="text-xl font-bold text-pink-600">
+              Calendar Integration
+            </div>
+            <div>
+              Seamlessly sync with popular calendar apps like Google Calendar,
+              Outlook, and Apple Calendar to ensure all your events are always
+              up-to-date across all devices.
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 items-center border-2 px-6 py-4 rounded-2xl">
+            <div className="bg-orange-100 p-4 rounded-full">
+              <Mail className="size-7 text-orange-500" />
+            </div>
+            <div className="text-xl font-bold text-orange-600">
+              Automated Reminders
+            </div>
+            <div>
+              Send automatic email and SMS reminders to attendees to reduce
+              no-shows. Customize reminder intervals to keep everyone informed
+              and prepared.
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 items-center border-2 px-6 py-4 rounded-2xl">
+            <div className="bg-green-100 p-4 rounded-full">
+              <Globe className="size-7 text-green-500" />
+            </div>
+            <div className="text-xl font-bold text-green-600">
+              Time Zone Detection
+            </div>
+            <div>
+              Automatically detect and adjust for different time zones, making
+              scheduling across regions hassle-free. Display all times in the
+              user’s local time for convenience.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/forms/ScheduleForm.tsx
@@ -0,0 +1,296 @@
+"use client";
+
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { scheduleFormSchema } from "@/schema/schedule";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants";
+import { timeToInt } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { formatTimezoneOffset } from "@/lib/formatters";
+import { Fragment, useEffect, useState } from "react";
+import { Loader2, Plus, X } from "lucide-react";
+import { Input } from "../ui/input";
+import { saveSchedule } from "@/server/actions/schedule";
+// import _ from "lodash";
+
+type Availability = {
+  startTime: string;
+  endTime: string;
+  dayOfWeek: (typeof DAYS_OF_WEEK_IN_ORDER)[number];
+};
+
+export function ScheduleForm({
+  schedule,
+}: {
+  schedule?: {
+    timezone: string;
+    availabilities: Availability[];
+  };
+}) {
+  const [successMessage, setSuccessMessage] = useState<string>();
+
+  useEffect(() => {
+    if (successMessage) {
+      setTimeout(() => {
+        setSuccessMessage("");
+      }, 2000);
+    }
+  }, [successMessage]);
+
+  const form = useForm<z.infer<typeof scheduleFormSchema>>({
+    resolver: zodResolver(scheduleFormSchema),
+    defaultValues: {
+      timezone:
+        schedule?.timezone ?? Intl.DateTimeFormat().resolvedOptions().timeZone,
+      availabilities: schedule?.availabilities.toSorted((a, b) => {
+        return timeToInt(a.startTime) - timeToInt(b.startTime);
+      }),
+    },
+  });
+
+  const {
+    append: addAvailability,
+    remove: removeAvailability,
+    fields: availabilityFields,
+  } = useFieldArray({
+    name: "availabilities",
+    control: form.control,
+  });
+
+  // Original Implementation
+  // const groupedAvailabilityFields = Object.groupBy(
+  //   availabilityFields.map((field, index) => ({ ...field, index })),
+  //   (availability) => availability.dayOfWeek
+  // );
+
+  // v1 Implementation
+  // const groupedAvailabilityFields = _.groupBy(
+  //   availabilityFields.map((field, index) => ({ ...field, index })),
+  //   "dayOfWeek"
+  // );
+
+  // v2 Implementation
+  // const groupedAvailabilityFields = _.groupBy(
+  //   availabilityFields.map((field, index) => ({ ...field, index })),
+  //   (availability) => availability.dayOfWeek
+  // );
+
+  // v3 Implementation
+  const groupedAvailabilityFields = availabilityFields
+    .map((field, index) => ({ ...field, index }))
+    .reduce((acc: any, availability) => {
+      const key = availability.dayOfWeek;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(availability);
+      return acc;
+    }, {});
+
+  // v4 Implementation
+  // const groupBy = (array: T[], key: (item: T) => string) =>
+  //   array.reduce((result: Record<string, T[]>, currentValue: T) => {
+  //     const groupKey = key(currentValue);
+  //     if (!result[groupKey]) {
+  //       result[groupKey] = [];
+  //     }
+  //     result[groupKey].push(currentValue);
+  //     return result;
+  //   }, {});
+
+  // const groupedAvailabilityFields = groupBy(
+  //   schedule.availabilities,
+  //   (availability) => availability.dayOfWeek
+  // );
+
+  async function onSubmit(values: z.infer<typeof scheduleFormSchema>) {
+    const data = await saveSchedule(values);
+    if (data?.error) {
+      form.setError("root", {
+        message: "There was an error saving your schedule",
+      });
+    } else {
+      setSuccessMessage("Schedule saved!");
+    }
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex gap-6 flex-col"
+      >
+        {form.formState.errors.root && (
+          <div className="text-destructive text-sm">
+            {form.formState.errors.root.message}
+          </div>
+        )}
+        {successMessage && (
+          <div className="text-green-500 text-sm">{successMessage}</div>
+        )}
+        <FormField
+          control={form.control}
+          name="timezone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Time Zone</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {Intl.supportedValuesOf("timeZone").map((timezone) => (
+                    <SelectItem key={timezone} value={timezone}>
+                      {timezone}
+                      {` (${formatTimezoneOffset(timezone)})`}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="grid grid-cols-[auto,1fr] gap-y-6 gap-x-4">
+          {DAYS_OF_WEEK_IN_ORDER.map((dayOfWeek) => (
+            <Fragment key={dayOfWeek}>
+              <div className="capitalize text-sm font-semibold">
+                {dayOfWeek.substring(0, 3)}
+              </div>
+              <div className="flex flex-col gap-2">
+                <Button
+                  type="button"
+                  className="size-6 p-1"
+                  variant={"outline"}
+                  onClick={() => {
+                    addAvailability({
+                      dayOfWeek,
+                      startTime: "9:00",
+                      endTime: "17:00",
+                    });
+                  }}
+                >
+                  <Plus className="size-full" />
+                </Button>
+                {groupedAvailabilityFields[dayOfWeek]?.map(
+                  (field: any, labelIndex: any) => (
+                    <div className="flex flex-col gap-1" key={field.id}>
+                      <div
+                        key={labelIndex}
+                        className="flex gap-2 items-center "
+                      >
+                        <FormField
+                          control={form.control}
+                          name={`availabilities.${field.index}.startTime`}
+                          render={({ field: formField }) => (
+                            <FormItem>
+                              <FormControl>
+                                <Input
+                                  className="w-24"
+                                  aria-label={`${dayOfWeek} Start Time ${
+                                    labelIndex + 1
+                                  }`}
+                                  {...formField}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        />
+                        -
+                        <FormField
+                          key={labelIndex}
+                          control={form.control}
+                          name={`availabilities.${field.index}.endTime`}
+                          render={({ field: formField }) => (
+                            <FormItem>
+                              <FormControl>
+                                <Input
+                                  className="w-24"
+                                  aria-label={`${dayOfWeek} End Time ${
+                                    labelIndex + 1
+                                  }`}
+                                  {...formField}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        />
+                        <Button
+                          type="button"
+                          className="size-6 p-1"
+                          variant={"destructiveGhost"}
+                          onClick={() => {
+                            removeAvailability(field.index);
+                          }}
+                        >
+                          <X />
+                        </Button>
+                      </div>
+                      <FormMessage>
+                        {
+                          form.formState.errors.availabilities?.at?.(
+                            field.index
+                          )?.root?.message
+                        }
+                      </FormMessage>
+                      <FormMessage>
+                        {
+                          form.formState.errors.availabilities?.at?.(
+                            field.index
+                          )?.startTime?.message
+                        }
+                      </FormMessage>
+                      <FormMessage>
+                        {
+                          form.formState.errors.availabilities?.at?.(
+                            field.index
+                          )?.endTime?.message
+                        }
+                      </FormMessage>
+                    </div>
+                  )
+                )}
+              </div>
+            </Fragment>
+          ))}
+        </div>
+
+        <div className="flex gap-2 justify-end">
+          <Button
+            disabled={
+              form.formState.isSubmitting || form.formState.isValidating
+            }
+            type="submit"
+          >
+            {form.formState.isSubmitting || form.formState.isValidating ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Saving..
+              </>
+            ) : (
+              <>Save</>
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
